6018bec4644fc79df1b15861d90f3fbea492c8ac
sun'an <98011546+way-loser@users.noreply.github.com>
2022-03-26 11:02:00 +0800

    Create linklist.h

---
+++ the first week/Linklist/Headers/linklist.h
@@ -0,0 +1,6 @@
+int setuplist(linkedlist *L);
+void traverlist(linkedlist L);
+int addlist(node *p, int data);
+int delete(linkedlist *L, int i, linkedlist *e);
+void destroy(linkedlist *L);
+int insert(node *L, int i, int e);
