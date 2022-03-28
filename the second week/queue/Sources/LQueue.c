ab7d842fefb72777abbe0e3488c79845ac5bb45b
sun'an <98011546+way-loser@users.noreply.github.com>
2022-03-28 23:23:52 +0800

    Add files via upload

---
+++ the second week/queue/Sources/LQueue.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "LQueue.h" 
+//#ifndef LQUEUE_H_INCLUDED
+//#define LQUEUE_H_INCLUDED
+
+/**
+ *  @name        : void InitLQueue(LQueue *Q)
+ *    @description : 初始化队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void InitLQueue(LQueue *Q){
+	Q->front = Q->rear = (Node*)malloc(sizeof(Node));
+	if (!Q->front)
+		return FALSE;
+	Q->front->next = NULL;
+	Q->length = 0;
+	return TRUE;
+}
+
+/**
+ *  @name        : void DestoryLQueue(LQueue *Q)
+ *    @description : 销毁队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void DestoryLQueue(LQueue *Q){
+	
+}
+
+/**
+ *  @name        : Status IsEmptyLQueue(const LQueue *Q)
+ *    @description : 检查队列是否为空
+ *    @param         Q 队列指针Q
+ *    @return         : 空-TRUE; 未空-FALSE
+ *  @notice      : None
+ */
+Status IsEmptyLQueue(const LQueue *Q){
+	
+}
+
+/**
+ *  @name        : Status GetHeadLQueue(LQueue *Q, void *e)
+ *    @description : 查看队头元素
+ *    @param         Q e 队列指针Q,返回数据指针e
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : 队列是否空
+ */
+Status GetHeadLQueue(LQueue *Q, void *e){
+	
+}
+
+/**
+ *  @name        : int LengthLQueue(LQueue *Q)
+ *    @description : 确定队列长度
+ *    @param         Q 队列指针Q
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : None
+ */
+int LengthLQueue(LQueue *Q){
+	
+}
+
+/**
+ *  @name        : Status EnLQueue(LQueue *Q, void *data)
+ *    @description : 入队操作
+ *    @param         Q 队列指针Q,入队数据指针data
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : 队列是否为空
+ */
+Status EnLQueue(LQueue *Q, void *data){
+	Node *p = (Node*)malloc(sizeof(Node));
+	if (!p)
+	{
+		printf("内存不足\n");
+		return FALSE;
+	}
+	p->next = NULL;
+	p->data = data;
+	Q->rear->next = p;
+	Q->rear = p;
+	Q->length++;
+	return TRUE;
+}
+
+/**
+ *  @name        : Status DeLQueue(LQueue *Q)
+ *    @description : 出队操作
+ *    @param         Q 队列指针Q
+ *    @return         : 成功-TRUE; 失败-FALSE
+ *  @notice      : None
+ */
+Status DeLQueue(LQueue* Q) {
+	if (Q->front == Q->rear) {
+		printf("栈队列内无元素\n");
+		return FALSE;
+	}
+	else {
+		Node* p = Q->front->next;
+		//p为第一个元素
+		Q->front->next = p->next;
+		if (p == Q->rear) {
+			//判断是否只有一个元素
+			Q->rear = Q->front;
+		}
+		Q->length--;
+		free(p);
+		return TRUE;
+	}
+}
+/**
+ *  @name        : void ClearLQueue(AQueue *Q)
+ *    @description : 清空队列
+ *    @param         Q 队列指针Q
+ *  @notice      : None
+ */
+void ClearLQueue(LQueue *Q){
+	while (Q->front != Q->rear)
+	{
+		DeLQueue(Q);
+	}
+	printf("队列清空完成\n");
+}
+
+/**
+ *  @name        : Status TraverseLQueue(const LQueue *Q, void (*foo)(void *q))
+ *    @description : 遍历函数操作
+ *    @param         Q 队列指针Q，操作函数指针foo
+ *    @return         : None
+ *  @notice      : None
+ */
+Status TraverseLQueue(const LQueue *Q, void (*foo)(void *q)){
+	Node* p = Q->front->next;//p为第一个元素
+	while (p)
+	{
+		printf("%d", p->data);
+	}
+}
+
+/**
+ *  @name        : void LPrint(void *q)
+ *    @description : 操作函数
+ *    @param         q 指针q
+ 
+ *  @notice      : None
+ */
+void LPrint(void *q){
+	
+}
+
+/**************************************************************
+ *    End-Multi-Include-Prevent Section
+ **************************************************************/
+#endif // LQUEUE_H_INCLUDED
+
+
---
+++ the second week/queue/Sources/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include "LQueue.h" 
+
+void main() {
+	LQueue Q ;
+	InitLQueue(&Q);
+
+	//return 0;
+}
